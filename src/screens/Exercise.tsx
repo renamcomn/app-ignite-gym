dd3476edf03f563d6e0f847a2445322ca435db64
Renan Henrique <renamcomn@Renans-Mac-mini.local>
2024-03-06 21:21:29 -0300

    ui adjust

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,10 +1,15 @@
 import { TouchableOpacity } from 'react-native';
-import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { Box, Heading, HStack, Icon, Image, Text, VStack, ScrollView } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-
-import BodySvg from '@assets/body.svg';
 import { AppNavigatorRoutesProps } from '@routes/AppRoutes';
+
+import ExerciseIMG from '@assets/exercise.png'
+import BodySvg from '@assets/body.svg';
+import SeriesSvg from '@assets/series.svg';
+import RepetitionsSvg from '@assets/repetitions.svg';
+
+import Button from '@components/Button';
 
 export function Exercise() {
 
@@ -15,6 +20,7 @@ export function Exercise() {
   }
 
   return (
+    
     <VStack flex={1}>
       <VStack px={8} bg="gray.600" pt={12}>
         <TouchableOpacity onPress={handleGoBack}>
@@ -40,6 +46,44 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
+
+      <ScrollView>
+        <VStack p={8}>
+          <Image
+            w="full"
+            h={80}
+            source={ExerciseIMG}
+            alt="Nome do exercício"
+            mb={3}
+            resizeMode="cover"
+            rounded="lg"
+          />
+
+          <Box bg="gray.600" rounded="md" pb={4} px={4}>
+            <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
+              <HStack>
+                <SeriesSvg />
+
+                <Text color="gray.200" ml="2">
+                  3 séries
+                </Text>
+              </HStack>
+
+              <HStack>
+                <RepetitionsSvg />
+                
+                <Text color="gray.200" ml="2">
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button 
+              title="Marcar como realizado"
+            />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   );
 }

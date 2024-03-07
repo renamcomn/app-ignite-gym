29e772c4ab7f6f67a8e31b17cfa44bc08c27dcc3
Renan Henrique <renamcomn@Renans-Mac-mini.local>
2024-03-06 21:09:18 -0300

    create screens interfaces and components

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,45 @@
-import { Center, Text } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import BodySvg from '@assets/body.svg';
+import { AppNavigatorRoutesProps } from '@routes/AppRoutes';
 
 export function Exercise() {
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon 
+            as={Feather}
+            name="arrow-left"
+            color="green.500"
+            size={6}
+          />
+        </TouchableOpacity>
+
+        <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
+          <Heading color="gray.100" fontSize="lg"  flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   );
 }
